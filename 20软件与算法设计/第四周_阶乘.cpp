659096bdbe7383fe5feb0a5642144e8edefad237
liangzuying <76089357+liangzuying@users.noreply.github.com>
2021-09-27 00:30:16 +0800

    Create 第四周_阶乘.cpp

---
+++ 20软件与算法设计/第四周_阶乘.cpp
@@ -0,0 +1,45 @@
+//递归求阶乘
+#include <iostream>
+using namespace std;
+
+long Factorial(long n) {
+	if (n==1)
+	{
+		return 1;
+	}
+	else
+	{
+		return n * (n - 1);
+	}
+}
+int main(){
+	long n;
+	cout << "请输入n的值：" << endl;
+	cin >> n;
+	cout <<"n的阶乘为：" << Factorial(n) << endl;
+	return 0;
+}
+
+
+
+//递推求阶乘
+#include <iostream>
+using namespace std;
+
+long Factorial(long n) {
+	long a=1;
+	
+		for (long i = 1; i <= n; i++)
+		{
+			a *= i;
+			
+		}
+	return a;
+}
+int main() {
+	long n;
+	cin >> n;
+	cout<<Factorial(n) << endl;
+	return 0;
+}
+//复杂度O(n),循环n次
